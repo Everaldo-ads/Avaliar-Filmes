d9c9fb94d28694f0a3728fe79be599a89d2a07b2
Everaldo451 <everaldo.work.jr@gmail.com>
2025-11-18 21:32:39 -0300

    feat: update user feature

--- api/users/create.php
+++ api/users/create.php
@@ -4,6 +4,7 @@
         $email = $_REQUEST['email'];
         $password = $_REQUEST['password'];
         $nickname = $_REQUEST['name'];
+        $profileImage = $_FILES['profileImage'];
 
         if (!$email) {
             echo "Erro ao criar usuário: email necessário."
@@ -12,7 +13,12 @@
         } else if (!$nickname) {
             echo "Erro ao criar usuário: nickname necessário."
         } else {
-            $sql = "INSERT INTO users (email, password, nickname) VALUES ('$email', '$password', '$nickname', '$role')";
+            $binaryImageData = file_get_contents($profileImage["tmp_name"]);
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "
+                INSERT INTO users (email, password, nickname, profile_image) 
+                VALUES ('$email', '$hashedPassword', '$nickname', '$binaryImageData');
+            ";
             $insert = mysqli_query($conn, $sql);
             if ($insert) {
                 echo "Usuário criado com sucesso!";
---
+++ api/users/delete.php
@@ -0,0 +1,30 @@
+<?php
+    include_once("../../db/config.inc.php");
+    if ($_SERVER["method"] == "POST") {
+        $id = $_REQUEST['id'];
+
+        if ($id) {
+            $sql = "SELECT id FROM users WHERE id=$id;";
+            $select = mysqli_query($conn, $sql);
+            
+            if ($select) {
+                $sql = "DELETE FROM users WHERE id=$id;";
+                $delete = mysqli_query($conn, $sql);
+                if ($delete) {
+                    echo "Usuário deletado com sucesso.";
+                }
+                else {
+                    echo "Erro ao deletar o usuário.";
+                }
+            } else {
+                echo "Usuário não encontrado";
+            }
+        }
+        else (!$id) {
+            echo "Erro ao deletar usuário: id necessário."
+        } 
+    } else {
+        echo "Método de requisição inválido.";
+    }
+    mysqli_close($conn);
+?>
