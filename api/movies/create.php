e86db3f1100015948f12e53d02dc460914ffd413
Everaldo451 <everaldo.work.jr@gmail.com>
2025-11-20 12:37:05 -0300

    feat: update movies create

--- api/movies/create.php
+++ api/movies/create.php
@@ -24,14 +24,29 @@
         $budget = $_REQUEST['budget'];
 
         $sql = "
-            INSERT INTO movie (age_classification, name, status, release_date, country, duration, budget); 
+            INSERT INTO movie (age_classification, name, status, release_date, country, duration, budget)
             VALUES ('$age_class', '$name', '$status', '$release_date', '$country', '$duration', '$budget');
         ";
         $insert = mysqli_query($conn, $sql);
-        if ($insert) {
-            echo "Usuário criado com sucesso!";
+
+        $movie_id = mysqli_insert_id($conn);
+        $movieImagesDir = $FILES["movieImages"]["tmp_name"];
+        $imagesCreated = true;
+        foreach ($movieImagesDir as $movieImage) {
+            $imageContent = file_get_contents($movieImage);
+            $sql = "INSERT INTO MovieImage (movie_id, content) VALUES ('$movie_id', '$imageContent')";
+            $image_insert = mysqli_query($conn, $sql);
+            if (!$image_insert) {
+                $imagesCreated = false;
+            }
+        }
+
+        if (!$imagesCreated) {
+            echo "Erro ao cadastrar as imagens do filme.";
+        } elseif (!$insert) {
+            echo "Erro ao criar filme";
         } else {
-            echo "Erro ao criar usuário";
+            echo "Erro ao criar filme";
         }
     } else {
         echo "Método de requisição inválido.";
