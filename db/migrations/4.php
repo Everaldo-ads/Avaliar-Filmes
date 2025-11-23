b97171593375293e6958da2a8bb0250b48300e75
Everaldo451 <everaldo.work.jr@gmail.com>
2025-11-23 16:59:54 -0300

    feat: add favorites item create

---
+++ db/migrations/4.php
@@ -0,0 +1,24 @@
+<?php
+    include_once("../config.inc.php");
+    
+    $sql = "CREATE TABLE IF NOT EXISTS `Favorites` (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT NOT NULL,
+
+        FOREIGN KEY (user_id) REFERENCES User(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS `FavoritesItem` (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        movie_id INT NOT NULL,
+        favorites_id INT NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+
+        FOREIGN KEY (movie_id) REFERENCES Movie(id)
+        FOREIGN KEY (favorites_id) REFERENCES Favorites(id)
+    );"
+
+    mysqli_query($conn, $sql);
+
+    $mysqli_close($conn);
+?>
