a377b483f234d98e191ff07cafbaac6d39f380b4
José Davi <davimauricio527@gmail.com>
2025-11-24 13:31:42 -0300

    Create delete.php

---
+++ api/review/delete.php
@@ -0,0 +1,35 @@
+<?php
+    include_once("../../db/config.inc.php");
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $id = $_REQUEST['id'];
+
+        if ($id) {
+            $sql = "SELECT id FROM Review WHERE id=$id;";
+            $select = mysqli_query($conn, $sql);
+            
+            if ($select && mysqli_num_rows($select) > 0) {
+
+                $sql = "DELETE FROM Review WHERE id=$id;";
+                $delete = mysqli_query($conn, $sql);
+
+                if ($delete) {
+                    echo "Review deletado com sucesso.";
+                } else {
+                    echo "Erro ao deletar a review.";
+                }
+
+            } else {
+                echo "Review não encontrada.";
+            }
+
+        } else {
+            echo "Erro ao deletar review: id necessário.";
+        }
+
+    } else {
+        echo "Método de requisição inválido.";
+    }
+
+    mysqli_close($conn);
+?>
