27fff785e0885fda55a3a250bdd0984d30c744dc
José Davi <davimauricio527@gmail.com>
2025-11-27 12:44:41 -0300

    não funciona

--- db/scripts/migrate.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $i=1;
-    $migrations_path = "../migrations";
-    while (file_exists($full_path = $migrations_path . "/$i.php")) {
-        include_once($full_path);
-        $i++;
-    }
-?>
