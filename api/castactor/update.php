83db672c28dbf0d083e3d4138f05671fe8343a7a
José Davi <davimauricio527@gmail.com>
2025-12-02 17:09:13 -0300

    Create update.php

---
+++ api/castactor/update.php
@@ -0,0 +1,31 @@
+<?php
+include_once("../../db/config.inc.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $id = $_POST['id'];
+    $cast_id = $_POST['cast_id'];
+    $actor_id = $_POST['actor_id'];
+
+    if (!$id) {
+        echo "Erro: ID é obrigatório.";
+        exit;
+    }
+
+    $sql = "UPDATE cast_actor SET 
+            cast_id = '$cast_id', 
+            actor_id = '$actor_id' 
+            WHERE id = '$id'";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Vínculo Ator-Elenco atualizado com sucesso!";
+    } else {
+        echo "Erro MySQL: " . mysqli_error($conn);
+    }
+
+} else {
+    echo "Método inválido.";
+}
+
+mysqli_close($conn);
+?>
