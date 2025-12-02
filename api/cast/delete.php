f67dbc6c0cfbd1322d52fdabcee3e0920a02379e
José Davi <davimauricio527@gmail.com>
2025-12-01 23:25:51 -0300

    Refactor delete.php for better error handling

--- api/cast/delete.php
+++ api/cast/delete.php
@@ -1,29 +1,38 @@
 <?php
-include_once("../../db/config.inc.php");
+    include_once("../../db/config.inc.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $id = $_REQUEST['id'];
 
-    $id = $_POST["id"];
+        if ($id) {
 
-    if (!$id) {
-        echo "Erro: id é obrigatório.";
-        exit;
-    }
+            
+            $sql = "SELECT id FROM castm WHERE id=$id;";
+            $select = mysqli_query($conn, $sql);
+            
+            if (mysqli_num_rows($select) > 0) {
 
-    mysqli_query($conn, "DELETE FROM CastActor WHERE cast_id=$id");
+                
+                $sql = "DELETE FROM castm WHERE id=$id;";
+                $delete = mysqli_query($conn, $sql);
 
-    $sql = "DELETE FROM Cast WHERE id=$id";
-    $delete = mysqli_query($conn, $sql);
+                if ($delete) {
+                    echo "Ator deletado com sucesso.";
+                } else {
+                    echo "Erro ao deletar o ator.";
+                }
 
-    if ($delete) {
-        echo "Cast deletado com sucesso.";
+            } else {
+                echo "Ator não encontrado";
+            }
+
+        } else {
+            echo "Erro ao deletar ator: id necessário.";
+        }
+
     } else {
-        echo "Erro ao deletar cast.";
+        echo "Método de requisição inválido.";
     }
 
-} else {
-    echo "Método inválido.";
-}
-
-mysqli_close($conn);
+    mysqli_close($conn);
 ?>
