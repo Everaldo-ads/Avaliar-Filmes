9cf0b7fa7078efcefbc9bfad55e85394710ac2da
José Davi <davimauricio527@gmail.com>
2025-12-03 00:00:04 -0300

    Create delete.php

---
+++ api/favorites/delete.php
@@ -0,0 +1,38 @@
+<?php
+    include_once("../../db/config.inc.php");
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $id = $_REQUEST['id'];
+
+        if ($id) {
+
+            
+            $sql = "SELECT id FROM favorites WHERE id=$id;";
+            $select = mysqli_query($conn, $sql);
+            
+            if (mysqli_num_rows($select) > 0) {
+
+                
+                $sql = "DELETE FROM favorites WHERE id=$id;";
+                $delete = mysqli_query($conn, $sql);
+
+                if ($delete) {
+                    echo "Ator deletado com sucesso.";
+                } else {
+                    echo "Erro ao deletar o ator.";
+                }
+
+            } else {
+                echo "Ator não encontrado";
+            }
+
+        } else {
+            echo "Erro ao deletar ator: id necessário.";
+        }
+
+    } else {
+        echo "Método de requisição inválido.";
+    }
+
+    mysqli_close($conn);
+?>
