f37a84fedb540af427a5b7d130443f3eb0db6722
José Davi <davimauricio527@gmail.com>
2025-12-03 14:39:14 -0300

    f

--- db/config.inc.php
+++ db/config.inc.php
@@ -1,10 +1,4 @@
 <?php
     $conn = mysqli_connect("localhost:3306","root","");
-    $db = mysqçli_select_db($conn, "database");
-
-    if ($conn) {
-        echo "Conexão com o banco de dados realizada com sucesso!";
-    } else {
-        echo "Falha na conexão com o banco de dados: ";
-    }
+    $db = mysqçli_select_db($conn, "cinephilia");
 ?>
